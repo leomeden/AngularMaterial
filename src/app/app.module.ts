8344687d4d0ef005314cc0273718b032c18975e2
leomeden <leonardomeden@gmail.com>
2022-04-16 11:57:56 -0300

    agregado de Formulario y cambio de theme material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Angular Materialga
-import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 // Componentes
 import { LoginComponent } from './components/login/login.component';
@@ -20,7 +22,9 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
