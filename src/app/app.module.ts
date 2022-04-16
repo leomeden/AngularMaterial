67a9888ba3ccf7076c4975c00c7a19aee7d2f1a5
leomeden <leonardomeden@gmail.com>
2022-04-16 13:50:09 -0300

    agregado validacion formulario y extraccion de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Angular Materialga
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 
 // Componentes
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule    
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  constructor (private fb: FormBuilder){
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  ingresar(){
+    console.log(this.form);
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if(usuario == 'lmeden' && password == 'admin123') {
+      //redireccionamos al dashboard
+    } else {
+      //mostramos un mensaje de error
+    }
+
   }
 
 }
