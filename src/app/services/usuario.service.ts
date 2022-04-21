8be202a9abd1b6c3f967e21cd1418f23baef8347
leomeden <leonardomeden@gmail.com>
2022-04-21 05:05:54 -0300

    completa funcionalidad crear usuario

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -26,4 +26,8 @@ export class UsuarioService {
   eliminarUsuario(index: number) {
     this.listUsuarios.splice(index, 1);
   }
+
+  agregarUsuario(usuario: Usuario) {
+    this.listUsuarios.unshift(usuario);
+  }
 }
