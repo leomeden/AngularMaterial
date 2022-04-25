e510f4f6f63fff29f79212939258d5e291125303
leomeden <leonardomeden@gmail.com>
2022-04-25 18:17:19 -0300

    agregado keys firebase en environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfqKuY8NF9fCBcxHRvG_vceopNb3IhS-w",
+    authDomain: "fir-auth-b50ee.firebaseapp.com",
+    projectId: "fir-auth-b50ee",
+    storageBucket: "fir-auth-b50ee.appspot.com",
+    messagingSenderId: "485331535071",
+    appId: "1:485331535071:web:9f3be6605cbfc4e0d3d56a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfqKuY8NF9fCBcxHRvG_vceopNb3IhS-w",
+    authDomain: "fir-auth-b50ee.firebaseapp.com",
+    projectId: "fir-auth-b50ee",
+    storageBucket: "fir-auth-b50ee.appspot.com",
+    messagingSenderId: "485331535071",
+    appId: "1:485331535071:web:9f3be6605cbfc4e0d3d56a"
+  }
 };
 
 /*
